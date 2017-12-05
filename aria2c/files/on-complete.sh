e32b1a3f787cc852750bd495759e2138300fcee6
ensonmj <ensonmj@gmail.com>
2017-12-05 19:18:26 +0800

    aria2c | fix start.sh

--- aria2c/files/on-complete.sh
+++ aria2c/files/on-complete.sh
@@ -5,4 +5,4 @@ if [ $2 -eq 1]; then
     rm -rf "$3.aria2"
 fi
 
-echo [$(date)] $2, $3, $1 "<br>" >> /data/log.html
+echo [$(date)] $2, $3, $1 "<br>" >> "${HOME}/Downloads/log.html"
--- aria2c/files/start.sh
+++ aria2c/files/start.sh
@@ -1,21 +1,27 @@
 #!/bin/sh
+
 if [ ! -f "${HOME}/.aria2/aria2.conf" ]; then
     cp "${HOME}/.config/aria2/aria2.conf" "${HOME}/.aria2/aria2.conf"
 fi
 
+if [ ! -f "${HOME}/.aria2/session.dat" ]; then
+    touch "${HOME}/.aria2/session.dat"
+fi
+
+# construct arguments
 arg=""
 if [ ! -d "${HOME}/Downloads/.dl" ]; then
     mkdir -p "${HOME}/Downloads/.dl"
 fi
-$arg += " -d ${HOME}/Downloads/.dl"
+arg="$arg -d ${HOME}/Downloads/.dl"
 
-if [ -n "${SECRET+x}" ]; then
-    $arg += " --rpc-secret=${SECRET}"
+if [ -n "${SECRET:+x}" ]; then
+    arg="$arg --rpc-secret=${SECRET}"
 fi
 
-if [ -n "${EXTERNAL_IP+x}" ]; then
-    $arg += " --bt-external-ip=${EXTERNAL_IP}"
+if [ -n "${EXTERNAL_IP:+x}" ]; then
+    arg="$arg --bt-external-ip=${EXTERNAL_IP}"
 fi
 
-echo "aria2c run with args[$arg]"
+echo "aria2c run with argments[$arg]"
 aria2c $arg
